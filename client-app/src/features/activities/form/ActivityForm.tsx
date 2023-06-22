e5b3bbdd1386546042912ec9f6f275c0dc39cef5
Izzat Ariz <izzatariz.2000@gmail.com>
2023-06-22 10:14:28 +0800

    3.1.5 Display Edit Form Based on Specific Activity

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import { ChangeEvent, useState } from "react";
 
 // ! Create Props to receive activities from App.tsx
 interface Props {
@@ -8,17 +9,79 @@ interface Props {
    activity: Activity | undefined;
 }
 
-export default function ActivityForm({ closeForm, activity }: Props) {
+export default function ActivityForm({
+   closeForm,
+   activity: selectedActivity,
+}: Props) {
+   // state
+   // ! if activity is null then {}
+   const initialState = selectedActivity ?? {
+      id: "",
+      title: "",
+      category: "",
+      description: "",
+      date: "",
+      city: "",
+      venue: "",
+   };
+
+   const [activity, setActivity] = useState(initialState);
+
+   // * Function to handle form submission
+   function handleSubmit() {
+      console.log(activity);
+   }
+
+   // * Function to handle input change
+   function handleInputChange(
+      event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+   ) {
+      // ! Track value inside form
+      const { name, value } = event.target;
+      // ! Spread existing property of activity, assign value to property with specific key of name
+      setActivity({ ...activity, [name]: value });
+   }
+
    return (
       // * Clear Float inside HTML
       <Segment clearing>
-         <Form>
-            <Form.Input placeholder="Title" />
-            <Form.TextArea placeholder="Description" />
-            <Form.Input placeholder="Category" />
-            <Form.Input placeholder="Date" />
-            <Form.Input placeholder="City" />
-            <Form.Input placeholder="Venue" />
+         <Form onSubmit={handleSubmit} autoComplete="off">
+            <Form.Input
+               placeholder="Title"
+               value={activity.title}
+               name="title"
+               onChange={handleInputChange}
+            />
+            <Form.TextArea
+               placeholder="Description"
+               value={activity.description}
+               name="description"
+               onChange={handleInputChange}
+            />
+            <Form.Input
+               placeholder="Category"
+               value={activity.category}
+               name="category"
+               onChange={handleInputChange}
+            />
+            <Form.Input
+               placeholder="Date"
+               value={activity.date}
+               name="date"
+               onChange={handleInputChange}
+            />
+            <Form.Input
+               placeholder="City"
+               value={activity.city}
+               name="city"
+               onChange={handleInputChange}
+            />
+            <Form.Input
+               placeholder="Venue"
+               value={activity.venue}
+               name="venue"
+               onChange={handleInputChange}
+            />
             <Button floated="right" positive type="submit" content="Submit" />
             <Button
                onClick={closeForm}
