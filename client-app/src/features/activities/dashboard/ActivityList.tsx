28abddc3d3cc083b08a65ae482fc8454351a088e
Izzat Ariz <izzatariz.2000@gmail.com>
2023-06-23 00:48:58 +0800

    3.2.4 Add Ability to Delete Activity

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { SyntheticEvent, useState } from "react";
 
 // * Interface for props
 interface Props {
@@ -8,6 +9,7 @@ interface Props {
    // * Function -> selectActivity returns void
    selectActivity: (id: string) => void;
    deleteActivity: (id: string) => void;
+   submitting: boolean;
 }
 
 // * Function
@@ -15,7 +17,18 @@ export default function ActivityList({
    activities,
    selectActivity,
    deleteActivity,
+   submitting,
 }: Props) {
+   const [target, setTarget] = useState("");
+
+   function handleActivityDelete(
+      e: SyntheticEvent<HTMLButtonElement>,
+      id: string
+   ) {
+      setTarget(e.currentTarget.name);
+      deleteActivity(id);
+   }
+
    return (
       <Segment>
          <Item.Group divided>
@@ -38,7 +51,9 @@ export default function ActivityList({
                            color="blue"
                         />
                         <Button
-                           onClick={() => deleteActivity(activity.id)}
+                           name={activity.id}
+                           loading={submitting && target === activity.id}
+                           onClick={(e) => handleActivityDelete(e, activity.id)}
                            floated="right"
                            content="Delete"
                            color="red"
